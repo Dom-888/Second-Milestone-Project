b0b34e8e2605b0c1e93d92d1ad3a84157a82e9fb
Dom-888 <spinelladomenico88@gmail.com>
2020-02-18 20:42:18 +0000

    Divide stylesheet in sections, rename classes for consistency and ease of reading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     // Run the following code as soon as the page is loaded
-    $("#introModal").modal('show');
+    $(".modal-introduction").modal('show');
 
     
 });
