732c78f3a32eb8b8e1cdc618e3dfe512e5fe08ee
Dom-888 <spinelladomenico88@gmail.com>
2020-02-19 22:37:14 +0000

    Fix: Add modal-dialog class to modal-introduction to allow this to close properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 
     // Run the following code as soon as the page is loaded
-    $(".modal-introduction").modal('show');
+    $(".modal-introduction").modal("show")
 
-    
+    $("#playButton").click(function(){
+        $(".modal-introduction").modal("hide");
+    });
+
+
+
 });
