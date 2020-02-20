b7792bbb07c4e88be777085f86330ea8e41a3482
Dom-888 <spinelladomenico88@gmail.com>
2020-02-20 23:53:36 +0000

    Rebuild modal from scrap
    The version of the modal I used previously had a number of problems, I started over with a new bootstrap modal to which I applied the following changes:
    - Disable click outside of the modal window to close the modal
    - Width: 85%, max-width 800px
    - Vertically center
    - Restore previous content and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // Run the following code as soon as the page is loaded
-    $(".modal-introduction").modal("show")
-
-    $("#playButton").click(function(){
-        $(".modal-introduction").modal("hide");
+    // Opens the modal dialog as soon as the page is loaded, also prevents modal closings by clicking outside the window
+    $("#introductionModal").modal({
+        backdrop: 'static',
+        keyboard: false
+    })
+    
+    $("#playButton").click(function () {
+        $("#selectionModal").modal("show");
     });
-
 
 
 });
