738aab74165d9371d08ee3a71288dee0d392a278
Dom-888 <spinelladomenico88@gmail.com>
2020-02-25 15:06:03 +0000

    Add all the themes to selectWord function

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,12 @@ $(document).ready(function () {
     });
 
     // Arrays containing the words to guess
-    const words = {
-        animals: ["ELEPHANT", "CROCODILE", "RABBIT", "EAGLE", "LION", "PANDA", "GIRAFFE", "FROG", "WHALE", "CHAMELEON", "OCTOPUS", "DOLPHIN", "ZEBRA", "WOLF", "FOX", "FALCON", "ARMADILLO", "BEETLE", "KOALA", "JAGUAR"],
-        movies: ["JURASSIC PARK", "THE BIG LEBOWSKI", "PULP FICTION", "THE WIZARD OF OZ", "FORREST GUMP", "JAWS", "APOCALYPSE NOW", "THE LORDS OF THE RINGS", "INCEPTION", "A CLOCKWORK ORANGE", "FULL METAL JACKET", "FIGHT CLUB", "THE MATRIX", "JOKER", "GLADIATOR", "BLADE RUNNER", "MAD MAX", "ALIEN", "BACK TO THE FUTURE", "STAR WARS"],
-        sports: ["FOOTBALL", "CRICKET", "BASKETBALL", "HOCKEY", "TENNIS", "VOLLEYBALL", "GOLF", "RUGBY", "BOXING", "SKIING", "ARCHERY", "HANDBALL", "FENCING", "MARATHON"],
-        cities: ["TIRANA", "VIENNA", "BRUSSELS", "SOFIA", "PRAGUE", "COPENHAGEN", "BERLIN", "ATHENS", "BUDAPEST", "AMSTERDAM", "MONACO", "LONDON", "ROME", "SYDNEY", "MOSCOW", "STOCKHOLM", "INSTANBUL", "DUBLIN", "HONG KONG", "NEW YORK", "RIO DE JANEIRO"],
-        jobs: ["SOMMELIER", "TAXI DRIVER", "PLUMBER", "DEVELOPER", "TRAINER", "TAILOR", "COOK", "TEACHER", "BARISTA", "BARBER", "FARMER", "LAWYER", "NURSE", "SURGEON", "ELECTRICIAN", "JOURNALIST", "FIREMAN", "ENGINEER", "SALESMAN", "LIFEGUARD", "DETECTIVE"],
-        // all = animals.concat(movies, sports, cities, jobs)
-    }
+    const animals = ["ELEPHANT", "CROCODILE", "RABBIT", "EAGLE", "LION", "PANDA", "GIRAFFE", "FROG", "WHALE", "CHAMELEON", "OCTOPUS", "DOLPHIN", "ZEBRA", "WOLF", "FOX", "FALCON", "ARMADILLO", "BEETLE", "KOALA", "JAGUAR"]
+    const movies = ["JURASSIC PARK", "THE BIG LEBOWSKI", "PULP FICTION", "THE WIZARD OF OZ", "FORREST GUMP", "JAWS", "APOCALYPSE NOW", "THE LORDS OF THE RINGS", "INCEPTION", "A CLOCKWORK ORANGE", "FULL METAL JACKET", "FIGHT CLUB", "THE MATRIX", "JOKER", "GLADIATOR", "BLADE RUNNER", "MAD MAX", "ALIEN", "BACK TO THE FUTURE", "STAR WARS"]
+    const sports = ["FOOTBALL", "CRICKET", "BASKETBALL", "HOCKEY", "TENNIS", "VOLLEYBALL", "GOLF", "RUGBY", "BOXING", "SKIING", "ARCHERY", "HANDBALL", "FENCING", "MARATHON"]
+    const cities = ["TIRANA", "VIENNA", "BRUSSELS", "SOFIA", "PRAGUE", "COPENHAGEN", "BERLIN", "ATHENS", "BUDAPEST", "AMSTERDAM", "MONACO", "LONDON", "ROME", "SYDNEY", "MOSCOW", "STOCKHOLM", "INSTANBUL", "DUBLIN", "HONG KONG", "NEW YORK", "RIO DE JANEIRO"]
+    const jobs = ["SOMMELIER", "TAXI DRIVER", "PLUMBER", "DEVELOPER", "TRAINER", "TAILOR", "COOK", "TEACHER", "BARISTA", "BARBER", "FARMER", "LAWYER", "NURSE", "SURGEON", "ELECTRICIAN", "JOURNALIST", "FIREMAN", "ENGINEER", "SALESMAN", "LIFEGUARD", "DETECTIVE"]
+    const all = animals.concat(movies, sports, cities, jobs)
 
     // Global variables
     let wordToGuess;
@@ -33,13 +31,29 @@ $(document).ready(function () {
         // Draws a random word from user selected theme and initiates the game
         selectWord: {
             animals: function () {
-                wordToGuess = words.animals[Math.floor(Math.random() * words.animals.length)];
+                wordToGuess = animals[Math.floor(Math.random() * animals.length)];
                 game.initiate()
             },
             movies: function () {
-                wordToGuess = words.movies[Math.floor(Math.random() * words.movies.length)];
+                wordToGuess = movies[Math.floor(Math.random() * movies.length)];
                 game.initiate()
-            }
+            },
+            sports: function () {
+                wordToGuess = sports[Math.floor(Math.random() * sports.length)];
+                game.initiate()
+            },
+            cities: function () {
+                wordToGuess = cities[Math.floor(Math.random() * cities.length)];
+                game.initiate()
+            },
+            jobs: function () {
+                wordToGuess = jobs[Math.floor(Math.random() * jobs.length)];
+                game.initiate()
+            },
+            all: function () {
+                wordToGuess = all[Math.floor(Math.random() * all.length)];
+                game.initiate()
+            },
         },
 
         initiate: function () {
@@ -47,12 +61,15 @@ $(document).ready(function () {
         }
     };
 
-    // Theme selection from user input
-    $("#animals").click(function () {
-        game.selectWord.animals();
-    });
-    $("#movies").click(function () {
-        game.selectWord.movies();
-    });
+    
+    /*---------------User inputs---------------*/
+
+    // Theme selection 
+    $("#animals").click(function () { game.selectWord.animals(); });
+    $("#movies").click(function () { game.selectWord.movies(); });
+    $("#sports").click(function () { game.selectWord.sports(); });
+    $("#cities").click(function () { game.selectWord.cities(); });
+    $("#jobs").click(function () { game.selectWord.jobs(); });
+    $("#all").click(function () { game.selectWord.all(); });
 
 });
