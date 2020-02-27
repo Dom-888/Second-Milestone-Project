76b9e19237bb425b62936f2c329e83400a9c192c
Dom-888 <spinelladomenico88@gmail.com>
2020-02-27 18:33:41 +0000

    Add HTML attributes to modals to prevent them from closing by clicking outside them, remove unnecessary JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,11 @@
 $(document).ready(function () {
 
-    // Opens the modal dialog as soon as the page is loaded, also prevents modal closings by clicking outside the window
-    $("#introductionModal").modal({
-        backdrop: 'static',
-        keyboard: false
-    })
-    // The closing of this dialog is handled in line, for now
-
+    // Opens the modal dialog as soon as the page is loaded
+    $("#introductionModal").modal("show")
 
     $("#playButton").click(function () {
-        $("#selectionModal").modal({
-            backdrop: 'static',
-            keyboard: false
-        });
         $("#introductionModal").modal("hide");
+        $("#selectionModal").modal("show");
     });
 
     // Arrays containing the words to guess
